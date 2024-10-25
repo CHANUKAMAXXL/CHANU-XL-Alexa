fda53591f02b006ab208e21f80b4a63bb953d870
CHANUKAMAXXL <chanukabigun1@gmail.com>
2024-10-25 11:54:16 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "QNhE3SiL#If2u4vxuzaf_7m0HWyJOaknE5N0XH4k8KOuyX55ihGU",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
